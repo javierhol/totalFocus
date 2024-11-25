2c2cf0b373aeb8a9544d63e07e76c34cec577b61
Javier Holguin <89085980+javierhol@users.noreply.github.com>
2024-11-24 22:25:52 -0500

    ADD: Implementar funcionalidad para agregar productos a la lista de deseos y gestionar duplicados

---
+++ add_wishlist.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'connect.php';
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$conn = getConnect();
+session_start();
+
+if(isset($_SESSION['user'])) {
+    $userId = $_SESSION['user'];
+    $productId = $data['id'];
+
+    $sql = "SELECT * FROM wishlist WHERE user_id = ? AND product_id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ii", $userId, $productId);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        echo json_encode(['success' => false, 'message' => 'El producto ya está en la lista de deseos']);
+    } else {
+        $insertSql = "INSERT INTO wishlist (user_id, product_id) VALUES (?, ?)";
+        $insertStmt = $conn->prepare($insertSql);
+        $insertStmt->bind_param("ii", $userId, $productId);
+
+        if ($insertStmt->execute()) {
+            echo json_encode(['success' => true, 'message' => 'Producto agregado a la lista de deseos']);
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Error al agregar el producto']);
+        }
+
+        $insertStmt->close();
+    }
+
+    
+    
+} else {
+    echo 'No hay datos de usuario en el localStorage';
+}
+
+?>
