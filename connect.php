6556aab9293e476182e0836f293380010e71f795
Javier Holguin <89085980+javierhol@users.noreply.github.com>
2024-11-24 22:26:17 -0500

    CAMBIAR: Mejorar la legibilidad del código en connect.php, ajustando el formato y el manejo de errores en la conexión a la base de datos

--- connect.php
+++ connect.php
@@ -1,15 +1,17 @@
 <?php
-function getConnect() {
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname = 'totalFocus';
+function getConnect()
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "totalFocus";
 
-    $conn = new mysqli( $servername, $username, $password, $dbname );
-    if ( $conn->connect_error ) {
-        throw new Exception( 'Connection failed: ' . $conn->connect_error );
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
     }
-    return $conn;
 
+    return $conn;
 }
 ?>
